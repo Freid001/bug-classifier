293c3822a7b3d945fd7437e94b6852b71a18247b
freid <freid@mediamath.com>
2019-07-05 16:57:48 +0100

    output base 64 img

--- src/app.py
+++ src/app.py
@@ -3,9 +3,9 @@ from starlette.responses import JSONResponse
 from starlette.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 from starlette.schemas import SchemaGenerator
-from starlette.requests import Request
 from io import BytesIO
 from pathlib import Path
+from base64 import b64encode
 from fastai.vision import (
     open_image,
     load_learner,
@@ -15,7 +15,6 @@ import sys
 import uvicorn
 import aiohttp
 import yaml
-import requests
 import logging
 
 path = Path(__file__).parent
@@ -147,24 +146,67 @@ async def classify_url(request):
     }, 400)
 
 # @app.route("/api/classify", methods=["POST"])
-# async def upload(request):
-#     data = await request.form()
-#     bytes = await (data["file"].read())
-#     return
-# predict_image_from_bytes(bytes)
+# async def classify_url(request):
+#     """
+#     summary: 'Classify an image binary.'
+#     responses:
+#         200:
+#             description: 'ok'
+#             content:
+#                 application/json:
+#                   schema:
+#                     $ref: '#/schemas/Classify'
+#         400:
+#             description: 'bad request'
+#             content:
+#                 application/json:
+#                   schema:
+#                     $ref: '#/schemas/Errors'
+#     """
+#     errors.clear()
+#     if 'url' in request.body.:
+#         bytes = await get_bytes(request.query_params['url'])
+#         if bytes is not False:
+#             return JSONResponse(predict_image_from_bytes(bytes), 200)
+#     else:
+#         errors.append({
+#             "message": "Url is required."
+#         })
+#
+#     return JSONResponse({
+#         "errors": errors
+#     }, 400)
 
 @app.route("/upload", methods=["GET"], include_in_schema=False)
 def upload(request):
     return templates.TemplateResponse('upload.html', {'request': request})
 
 @app.route("/upload", methods=["POST"], include_in_schema=False)
-def upload(request):
-    return templates.TemplateResponse('upload.html', {'request': request})
+async def upload(request):
+    errors.clear()
+    context = {'request': request}
+
+    # try:
+    data = await request.form()
+
+    logging.info(data)
+
+    bytes = await (data["file"].read())
+    context.update({'image': b64encode(bytes).decode('utf8')})
+    context.update({'prediction': predict_image_from_bytes(bytes)})
+    # except:
+    #     errors.append({
+    #         "message": "Error reading file."
+    #     })
+
+    # logging.info(bytes)
+    # logging.info(b64encode(bytes).decode('utf8'))
+
+    return templates.TemplateResponse('upload.html', context)
 
 @app.route("/url", methods=["GET"], include_in_schema=False)
 async def url(request):
     errors.clear()
-
     context = {'request': request}
 
     if 'url' in request.query_params:
